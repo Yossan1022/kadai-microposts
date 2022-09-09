b2c92ecbe7664f278b375eabe62e32f43856bca1
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-09-09 14:11:12 +0000

    w

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,16 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
 
  def create
-    @user = current_user.likes.create( params[:microposts_id])
+    @user = current_user.likes.create( params[:favorites_id])
+    current_user.like(micropost)
     flash[:success] = 'お気に入りに追加しました。'
     
-    redirect_to @user
+    redirect_to user
   end
 
   def destroy
     
-    user = User.find(params[:micropost_id])
+    user = User.find(params[:favorites_id])
     current_user.unlike(user)
     flash[:success] = 'ユーザのお気に入りを解除しました。'
     redirect_to user
